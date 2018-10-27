37adb882bc394d21eca101d35f78970cafe3ceab
Dustin Walker <dustinwalkerart@gmail.com>
2018-10-27 14:15:23 +0900

    added keys

---
+++ src/config/keys.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAHAgOk9q89vzKN83CCNhsFvJ5SvFFIWMQ",
+  authDomain: "movieapp-feef4.firebaseapp.com",
+  databaseURL: "https://movieapp-feef4.firebaseio.com",
+  projectId: "movieapp-feef4",
+  storageBucket: "movieapp-feef4.appspot.com",
+  messagingSenderId: "297083841884"
+};
