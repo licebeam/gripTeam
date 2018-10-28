5937a0bc6206636f67b82b86ce4e4e17288e928a
msherman0801 <38023883+msherman0801@users.noreply.github.com>
2018-10-28 14:06:22 -0400

    Minor styling changes

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 const Container = styled.div`
-  background-color: pink;
   height: 100%;
   width: 100%;
   color: white;
   overflow: auto;
+  cursor: pointer;
   display: flex;
   flex-direction: column;
 `
 const Genre = styled.div`
-  width: 100%;
-  background-color: white;
+  width: 300px;
+  margin: auto;
+  background-color: #d9e4e5;
   color: black;
   height: 100px;
+  cursor: pointer;
   &:hover{
-    background-color: grey;
+    background-color: #c5d0d1;
   }
 `
 class GenreList extends Component {
