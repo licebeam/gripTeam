b90ba7aef50148f23d48dcaabbc1f6e07d0a6b1f
Dustin Walker <dustinwalkerart@gmail.com>
2018-10-28 15:48:35 +0900

    updated and taught redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,3 +2,8 @@ export const changeText = text => ({
   type: 'CHANGE_TEXT',
   text,
 })
+
+export const changeCraig = text => ({
+  type: 'CHANGE_CRAIG',
+  text,
+})
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,22 +2,32 @@ const reduceAction = (
   state = {
     text: 'hey whats up',
     list: [],
-    highestRated: [
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-      { title: 'Genre', rating: 100 },
-    ]
+    genres: [
+      { title: 'Horror' },
+      { title: 'Action' },
+      { title: 'Comedy' },
+      { title: 'Romance' },
+      { title: 'Sci-Fi' },
+      { title: 'Documentary' },
+      { title: 'Genre' },
+      { title: 'Genre' },
+      { title: 'Genre' },
+    ],
+    movieList: [
+      { title: 'Cool Movie', rating: 100 },
+      { title: 'Bad Movie', rating: 100 },
+      { title: 'Cool Movie', rating: 100 },
+      { title: 'Cool Movie', rating: 100 },
+      { title: 'Cool Movie', rating: 100 },
+      { title: 'Cool Movie', rating: 100 },
+    ],
   },
   action
 ) => {
   switch (action.type) {
     case 'CHANGE_TEXT':
+      return Object.assign({}, state, { text: action.text })
+    case 'CHANGE_CRAIG':
       return Object.assign({}, state, { text: action.text })
     default: return state;
   }
